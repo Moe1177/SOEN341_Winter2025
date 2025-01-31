826c0e31be89ec44c8af3b53075e22c59dd958c3
Mohamad Addasi <maddasi04@gmail.com>
2025-01-31 17:28:22 -0500

    change frontend folder name and create spring boot backend project

---
+++ soen341-backend/src/main/java/com/example/soen341_backend/Soen341BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.soen341_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soen341BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Soen341BackendApplication.class, args);
+	}
+
+}
---
+++ soen341-backend/src/test/java/com/example/soen341_backend/Soen341BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soen341_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Soen341BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
