8ded0ff9c8ef08eb2fbafb3f3cfe07d0d9538f66
Fouad Elian <elianfouad@gmail.com>
2025-02-28 16:49:59 -0500

    Full landing page for the application

--- soen341-frontend/app/page.tsx
+++ soen341-frontend/app/page.tsx
@@ -1,5 +1,29 @@
+import Features from "@/Components/Features";
+import Hero from "@/Components/Hero";
+import About from "@/Components/About";
 import Image from "next/image";
+import SignUp from "@/Components/SignUp";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+    <div id="Hero">
+    <Hero/>
+    </div>
+    <div id="About">
+    <About/>
+    </div>
+    
+    <div id="Features">
+    <Features/>
+    </div>
+    
+    <div id="SignUp">
+    <SignUp/>
+    </div>
+    
+    
+    </>
+   
+    )
 }
