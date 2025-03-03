564ac21503074a4907abed188e5904a4c38113b3
YoussefYassa7112 <149968920+YoussefYassa7112@users.noreply.github.com>
2025-03-02 20:32:29 -0500

    Created a new branch to integrate backend authentication to the Login

---
+++ soen341-frontend/app/direct-messaging/page.tsx
@@ -0,0 +1,11 @@
+import  Footer  from "@/Components/footer";
+import { SidebarDemo } from "@/Components/sidemenu";
+
+export default function direct_messaging() {
+  return (
+    <div className="flex flex-col">
+      <SidebarDemo />
+      <Footer/>
+    </div>
+  );
+}
