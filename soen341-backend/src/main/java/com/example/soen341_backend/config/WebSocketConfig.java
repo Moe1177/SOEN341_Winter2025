2de08521707cc8af974efed78743fbc2252aac2d
Moe1177 <maddasi04@gmail.com>
2025-03-10 02:19:45 -0400

    Add frontend URL as an allowed origin

--- soen341-backend/src/main/java/com/example/soen341_backend/config/WebSocketConfig.java
+++ soen341-backend/src/main/java/com/example/soen341_backend/config/WebSocketConfig.java
@@ -38,7 +38,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+    registry
+        .addEndpoint("/ws")
+        .setAllowedOrigins("http://localhost:3000", "https://test-messages.vercel.app")
+        .withSockJS()
+        .setSuppressCors(true);
   }
 
   @Override
